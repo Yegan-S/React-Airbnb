b4f046f8af16201ec6e0d7d7ec07a40acb180254
sedi <salehisediqe86@gmail.com>
2023-06-16 08:35:36 -0700

    Upgrade Sold Out Badge

--- src/Card.js
+++ src/Card.js
@@ -3,10 +3,15 @@ import Starimage from "./images/star.png";
 
 
 export default function Card(props){
-    
+    let badgeText
+    if (props.openSpots === 0){
+        badgeText = "SOLD OUT"
+    } else if(props.location === "Online"){
+        badgeText = "ONLINE"
+    }
     return(
         <div className="Card">
-            {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
+            {badgeText && <div className="card--badge">{badgeText}</div>}
             <img src={`../image/${props.img}`}
             alt="img" className="card--image"/>
             <div className="card--stats">
