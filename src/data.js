40bd4745e768eedb5d0b9136c0efcf9f1d42602d
sedi <salehisediqe86@gmail.com>
2023-06-21 08:54:50 -0700

    changed data.js in order to fixed a warning

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-export default [
+const data = [
     {
         id:1,
         title:"Life Lessons with Katie Zaferes",
@@ -38,6 +38,7 @@ export default [
         location:"Norway",
          openSpots:3,
 
-    }
-]
+    },
+] ;
+export default data;
 
